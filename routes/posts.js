d5453e98580b8b2631d1bc08ff8f7d6b442d1d49
Geerocktricks <geerockface4@gmail.com>
2022-07-07 13:50:25 +0300

    Completes the single post route

--- routes/posts.js
+++ routes/posts.js
@@ -19,13 +19,17 @@ router.post('/', async (req, res) => {
 
 // Get all posts
 router.get('/', (req, res) => {
-    res.json({success: true, allPosts})
 })
 
 
 // Get single post
-router.get('/:id', (req, res) => {
-
+router.get('/:id',async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post)
+    } catch(err) {
+        res.status(500).json({ success: false, err})
+    }
 })
 
 // Update post
