7f41fb225fb87489f62634a46546a1c6fb0c8e0e
Geerocktricks <geerockface4@gmail.com>
2022-07-07 13:26:26 +0300

    Completes the update post route

--- routes/posts.js
+++ routes/posts.js
@@ -2,11 +2,14 @@ const router = require("express").Router();
 const User = require('../Models/user');
 const Post = require('../Models/post');
 
+let allPosts = [];
+
 // Create post
 router.post('/', async (req, res) => {
     const newPost = new Post(req.body);
     try {
         const savedPost = await newPost.save();
+        allPosts.push(newPost)
         res.json({ success: true, msg: 'Post created succesfully', user: savedPost})
 
     } catch(err) {
@@ -14,10 +17,42 @@ router.post('/', async (req, res) => {
     }
 })
 
-// Get post
+// Get all posts
+router.get('/', (req, res) => {
+})
+
+
+// Get single post
+router.get('/:id', (req, res) => {
+
+})
 
 // Update post
+router.patch('/:id', async (req, res) => {
+    try {
+        // Find the post
+        const post = await Post.findById(req.params.id);
+        // I'll change it to if user is logged in
+        if(post.author === req.body.author) {
+            try {
+                const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
+                    $set: req.body,
+                }, { new: true});
+                res.status(200).json({ success: true, updatedPost})
+            } catch(err) {
+                res.status(500).json({ success: false, msg: err});
+            }
+        } else {
+            res.status(401).json({ success: false, msg: 'You can update only your posts!'});
+        }
+    } catch(err) {
+        res.status(500).json({ success: false, msg: err});
+    }
+})
 
 // Delete post
+router.delete('/:id', (req, res) => {
+
+})
 
 module.exports = router;
