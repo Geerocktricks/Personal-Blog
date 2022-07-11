a96f8fda3062fe81604e65148341c978e265a185
Geerocktricks <geerockface4@gmail.com>
2022-07-11 15:38:05 +0300

    Completes protected request routes

--- blogClient/src/app/components/profile/profile.component.ts
+++ blogClient/src/app/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { UserInterface } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  user!: UserInterface;
+  constructor(private auth: AuthService, private router: Router) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.auth.getProfile().subscribe(profileData => {
+      this.user = profileData.user;
+    },
+    err => {
+      console.error(err)
+    })
   }
 
 }
--- blogClient/src/app/services/auth.service.ts
+++ blogClient/src/app/services/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { UserInterface } from '../interfaces/user';
 import { Observable } from 'rxjs/internal/Observable';
 
 let httpOtions = {
@@ -30,6 +29,21 @@ export class AuthService {
     return this.http.post<any>(`${this.baseUrl}/authenticate/login`, user, httpOtions)
   }
 
+
+  getProfile() {
+    let httpOtions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+        'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
+        'Authorization': `${this.authToken}`
+    })
+    }
+    this.loadToken();
+    return this.http.get<any>(`${this.baseUrl}/users/profile`, httpOtions)
+  }
+
   storeUserData(token: any, user: any) {
     // Store token in local storage
     localStorage.setItem('id_token', token);
@@ -37,6 +51,11 @@ export class AuthService {
     localStorage.setItem('user', JSON.stringify(user));
     this.authToken = token;
     this.user = user;
+  }
+
+  loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token
   }
 
   logout() {
